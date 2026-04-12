1b1dfccd5b9dda1e6a596802811328c3144e384a
Abdelrahman Salem <abubakir1997@gmail.com>
2026-04-12 13:26:05 -0400

    feat: add preset picker hotkeys

--- app/components/preset-picker-button.tsx
+++ app/components/preset-picker-button.tsx
@@ -1,14 +1,19 @@
 import { usePresetPicker } from "@codecanon/next-presets";
 import { Palette } from "lucide-react";
+import { useHotkeys } from 'react-hotkeys-hook'
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { MOD, SHIFT } from "@/lib/symbols";
+import { KbdGroup, Kbd } from "@/components/ui/kbd";
 
 function PresetPickerButton() {
   const { toggleOpen } = usePresetPicker();
+
+  useHotkeys('mod+shift+p', toggleOpen, { enableOnFormTags: true })
 
   return (
     <Tooltip>
@@ -20,7 +25,14 @@ function PresetPickerButton() {
           <Palette className="size-5" />
         </Button>
       </TooltipTrigger>
-      <TooltipContent>Choose Preset</TooltipContent>
+      <TooltipContent>
+        Choose Preset{" "}
+        <KbdGroup className="">
+          <Kbd>{MOD}</Kbd>
+          <Kbd>{SHIFT}</Kbd>
+          <Kbd>{"P"}</Kbd>
+        </KbdGroup>
+      </TooltipContent>
     </Tooltip>
   );
 }
